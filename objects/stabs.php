b569ef528a5a7d59a877597a4ba9266b4554d963
Ryuki0530 <159127441+Ryuki0530@users.noreply.github.com>
2024-09-24 16:47:37 +0900

    スタブオブジェクト用のファイルを作成 天気情報オブジェクトのスタブを作成

---
+++ objects/stabs.php
@@ -0,0 +1,23 @@
+<?php
+//このコードはスタブです。
+
+
+/*weatherInfoクラス
+getWeatherで天気情報を取得可能
+引数時間後の天気を表示
+引数無し→現在の天気
+*/
+class weatherInfo extends ubiObj{
+    private $manager;
+
+    public function __construct($name, $manager) {
+        echo("weatherInfo Start \n");
+
+
+    }
+    
+    public function getWeather($hour){
+        echo("");
+    }
+}
+?>
